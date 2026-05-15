bd915d6c89021f27a75b11a6683f63dc01a11060
tofolia61 <toffolia61@gmail.com>
2026-05-15 12:13:39 -0300

    Continuação Frontend JS

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-function executarSistema() {
-    const nome = document.getElementByld("inputNome").value;
+function executarSistema(){
+    //  Dados de Entrada
+    const nome = document.getElementById("inputNome").value;
+    const idade = parseInt(document.getElementById("inputIdade").value);
+    const valor = parseFloat(document.getElementById("inputValor").value);
+    const cupom = document.getElementById("inputCupom").value === "true";
+
+    //  Dados de saída
+    const msg = document.getElementById("mensagem-autorizacao");
+    const lista = document.getElementById("lista-estoque");
+    const relatorio = document.getElementById("relatorio-final");
+
+    //  Validação para campos vazios
+    if(!nome || isNaN(idade) || isNaN(valor)){
+        alert("Por favor, preencha todos os campos!");
+        return;
+    }
+
+    // Regras de negócio
+    if(idade >= 16) {
+        msg.innerText = `🕵️‍♂️venda autorizada🕵️‍♂️ ${nome}`;
+        msg.style.color = "#ffffff";
+
+        // Desconto
+        let ValorFinal = (Valor > 500 || cupom) ? valor * 0.85 : valor
+
+        //Estoque
+        let estoque = ["Placa de Video", "Processador", "Memória Ram"];
+        lista.innerHTML = ""; // Limpa a lista anterior
+
+
+    }
 }
